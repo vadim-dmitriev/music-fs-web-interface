dbbf11a4f2cda406f4b7a00a0f0e0229e46681a3
Vadim Dmitriev <dva3213@gmail.com>
2019-10-31 10:52:51 +0300

    Вынес логику работы с директорией в package dir

--- main.go
+++ main.go
@@ -1,19 +1,9 @@
 package main
 
 import (
-	"os"
-	"path/filepath"
-
 	"github.com/vadim-dmitriev/music-fs-web-interface/common"
+	"github.com/vadim-dmitriev/music-fs-web-interface/dir"
 )
-
-// Node является представлением вершины дерева директории
-type Node struct {
-	FullPath string
-	Name     string
-	Children []*Node
-	Parent   *Node
-}
 
 func main() {
 	cfg, err := common.NewConfig()
@@ -21,46 +11,9 @@ func main() {
 		panic(err)
 	}
 
-	_, err = newTree(cfg.MusicDir)
+	_, err = dir.NewTree(cfg.MusicDir)
 	if err != nil {
 		panic(err)
 	}
 
-}
-
-func newTree(rootDir string) (result *Node, err error) {
-	absRoot, err := filepath.Abs(rootDir)
-	if err != nil {
-		return
-	}
-	parents := make(map[string]*Node)
-
-	err = filepath.Walk(absRoot, func(path string, info os.FileInfo, err error) error {
-		if err != nil {
-			return err
-		}
-		parents[path] = &Node{
-			Name:     info.Name(),
-			FullPath: path,
-			Children: make([]*Node, 0),
-		}
-		return nil
-	})
-
-	if err != nil {
-		return
-	}
-
-	for path, node := range parents {
-		parentPath := filepath.Dir(path)
-
-		parent, exists := parents[parentPath]
-		if !exists { // Если нет родителя, то это корень.
-			result = node
-		} else {
-			node.Parent = parent
-			parent.Children = append(parent.Children, node)
-		}
-	}
-	return
 }
